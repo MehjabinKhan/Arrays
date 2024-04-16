8e1cae1f4a45eddb27111a041b09535de20452ba
MehjabinKhan <mehjabinkhan2004@gmail.com>
2024-04-16 22:20:10 +0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,12 @@
+"use strict";
+let fruitChaat = ["banana", "apple", "orange"];
+console.log(fruitChaat);
+// .push()
+fruitChaat.push('melon');
+console.log(fruitChaat);
+// .pop()
+fruitChaat.pop();
+console.log(fruitChaat);
+// .concat()
+fruitChaat = fruitChaat.concat(['melon', "kiwi"]);
+console.log(fruitChaat);
